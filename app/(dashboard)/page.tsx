98a8a02ce784fd34c8df74a7b24d5b6baa3eaf93
Kaushik Sharma <kaushik445566@gmail.com>
2024-07-27 13:45:13 +0530

    feat: authenitcation setup

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from '@/components/ui/button'
-import Image from 'next/image'
-
+import { UserButton } from '@clerk/nextjs'
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <Button>Click me</Button>
+            <UserButton afterSwitchSessionUrl="/" />
         </main>
     )
 }
