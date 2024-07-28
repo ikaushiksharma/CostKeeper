9c9257536b1e71e5b62fac7d0ea275c0c51ed90a
Kaushik Sharma <kaushik445566@gmail.com>
2024-07-28 19:16:54 +0530

    feat: categories ui

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,7 @@
-import { pgTable, text } from 'drizzle-orm/pg-core'
+import { relations } from 'drizzle-orm'
+import { integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { createInsertSchema } from 'drizzle-zod'
-
+import { z } from 'zod'
 export const accounts = pgTable('accounts', {
     id: text('id').primaryKey(),
     name: text('name').notNull(),
