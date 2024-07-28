dc19d19d6ebe44c7fa9bb1f4bc34e83cea2ca1d8
Kaushik Sharma <kaushik445566@gmail.com>
2024-07-28 18:42:15 +0530

    feat: delete individual account

--- features/accounts/components/edit-account-sheet.tsx
+++ features/accounts/components/edit-account-sheet.tsx
@@ -10,9 +10,10 @@ import {
 } from '@/components/ui/sheet'
 import { insertAccountSchema } from '@/db/schema'
 import { useEditAccount } from '@/features/accounts/api/use-edit-account'
+import { useDeleteAccount } from '@/features/accounts/api/use-delete-account'
 import { useGetAccount } from '@/features/accounts/api/use-get-account'
 import { useOpenAccount } from '@/features/accounts/hooks/use-open-account'
-
+import { useConfirm } from '@/hooks/use-confirm'
 import { AccountForm } from './account-form'
 
 const formSchema = insertAccountSchema.pick({
@@ -23,11 +24,15 @@ type FormValues = z.infer<typeof formSchema>
 
 export const EditAccountSheet = () => {
     const { isOpen, onClose, id } = useOpenAccount()
-
+    const [ConfirmDialog, confirm] = useConfirm(
+        'Are you sure?',
+        'You are about to delete this account.'
+    )
     const accountQuery = useGetAccount(id)
     const editMutation = useEditAccount(id)
+    const deleteMutation = useDeleteAccount(id)
 
-    const isPending = editMutation.isPending
+    const isPending = editMutation.isPending || deleteMutation.isPending
     const isLoading = accountQuery.isLoading
 
     const onSubmit = (values: FormValues) => {
@@ -46,30 +51,45 @@ export const EditAccountSheet = () => {
               name: '',
           }
 
+    const onDelete = async () => {
+        const ok = await confirm()
+
+        if (ok) {
+            deleteMutation.mutate(undefined, {
+                onSuccess: () => {
+                    onClose()
+                },
+            })
+        }
+    }
     return (
-        <Sheet open={isOpen || isPending} onOpenChange={onClose}>
-            <SheetContent className="space-y-4">
-                <SheetHeader>
-                    <SheetTitle>Edit Account</SheetTitle>
+        <>
+            <ConfirmDialog />
+            <Sheet open={isOpen || isPending} onOpenChange={onClose}>
+                <SheetContent className="space-y-4">
+                    <SheetHeader>
+                        <SheetTitle>Edit Account</SheetTitle>
 
-                    <SheetDescription>
-                        Edit an existing account.
-                    </SheetDescription>
-                </SheetHeader>
+                        <SheetDescription>
+                            Edit an existing account.
+                        </SheetDescription>
+                    </SheetHeader>
 
-                {isLoading ? (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                        <Loader2 className="size-4 text-muted-foreground animate-spin" />
-                    </div>
-                ) : (
-                    <AccountForm
-                        id={id}
-                        defaultValues={defaultValues}
-                        onSubmit={onSubmit}
-                        disabled={isPending}
-                    />
-                )}
-            </SheetContent>
-        </Sheet>
+                    {isLoading ? (
+                        <div className="absolute inset-0 flex items-center justify-center">
+                            <Loader2 className="size-4 text-muted-foreground animate-spin" />
+                        </div>
+                    ) : (
+                        <AccountForm
+                            id={id}
+                            defaultValues={defaultValues}
+                            onSubmit={onSubmit}
+                            disabled={isPending}
+                            onDelete={onDelete}
+                        />
+                    )}
+                </SheetContent>
+            </Sheet>
+        </>
     )
 }
