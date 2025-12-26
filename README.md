7ee1f79f72c70fcb9788a3e51ce473b9b6051543
Kaushik Sharma <kaushik445566@gmail.com>
2025-12-26 18:26:30 +0530

    feat: date range picker with presets

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Cost Keeper
+
 ## Flow Diagram
+
 ![diagram](https://github.com/user-attachments/assets/d53d01f6-80a5-46a3-951b-2d33cd2ba60f)
--- components/date-filter.tsx
+++ components/date-filter.tsx
@@ -1,22 +1,10 @@
 'use client'
 
-import { format, parseISO, startOfMonth, subDays } from 'date-fns'
-import { ChevronDown } from 'lucide-react'
+import { format, startOfMonth } from 'date-fns'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import qs from 'query-string'
-import { useState } from 'react'
-import { type DateRange } from 'react-day-picker'
 
-import { Button } from '@/components/ui/button'
-import { Calendar } from '@/components/ui/calendar'
-import {
-    Popover,
-    PopoverClose,
-    PopoverContent,
-    PopoverTrigger,
-} from '@/components/ui/popover'
-import { useGetSummary } from '@/features/summary/api/use-get-summary'
-import { cn, formatDateRange } from '@/lib/utils'
+import { DateRangePicker } from '@/components/ui/date-range-picker'
 
 export const DateFilter = () => {
     const pathname = usePathname()
@@ -30,17 +18,14 @@ export const DateFilter = () => {
     const defaultTo = new Date()
     const defaultFrom = startOfMonth(defaultTo)
 
-    const paramState = {
-        from: from ? new Date(from) : defaultFrom,
-        to: to ? new Date(to) : defaultTo,
-    }
-
-    const [date, setDate] = useState<DateRange | undefined>(paramState)
-
-    const pushToUrl = (dateRange: DateRange | undefined) => {
+    const handleUpdate = ({
+        range,
+    }: {
+        range: { from: Date; to: Date | undefined }
+    }) => {
         const query = {
-            from: format(dateRange?.from || defaultFrom, 'yyyy-MM-dd'),
-            to: format(dateRange?.to || defaultTo, 'yyyy-MM-dd'),
+            from: format(range.from || defaultFrom, 'yyyy-MM-dd'),
+            to: format(range.to || defaultTo, 'yyyy-MM-dd'),
             accountId,
         }
 
@@ -55,60 +40,13 @@ export const DateFilter = () => {
         router.push(url)
     }
 
-    const onReset = () => {
-        setDate(undefined)
-        pushToUrl(undefined)
-    }
-
     return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    disabled={false}
-                    size="sm"
-                    variant="outline"
-                    className="lg:w-auto w-full h-9 rounded-md px-3 font-normal bg-white/10 hover:bg-white/30 hover:text-white border-none focus:ring-offset-0 focus:ring-transparent outline-none text-white focus:bg-white/30 transition"
-                >
-                    <span>{formatDateRange(paramState)}</span>
-
-                    <ChevronDown className="ml-2 size-4 opacity-50" />
-                </Button>
-            </PopoverTrigger>
-
-            <PopoverContent className="lg:w-auto w-full p-0" align="start">
-                <Calendar
-                    disabled={false}
-                    initialFocus
-                    mode="range"
-                    defaultMonth={date?.from}
-                    selected={date}
-                    onSelect={setDate}
-                    numberOfMonths={2}
-                />
-
-                <div className="p-4 w-full flex items-center gap-x-2">
-                    <PopoverClose asChild>
-                        <Button
-                            onClick={onReset}
-                            disabled={!date?.from || !date?.to}
-                            className="w-full"
-                            variant="outline"
-                        >
-                            Reset
-                        </Button>
-                    </PopoverClose>
-
-                    <PopoverClose asChild>
-                        <Button
-                            onClick={() => pushToUrl(date)}
-                            disabled={!date?.from || !date?.to}
-                            className="w-full"
-                        >
-                            Apply
-                        </Button>
-                    </PopoverClose>
-                </div>
-            </PopoverContent>
-        </Popover>
+        <DateRangePicker
+            initialDateFrom={from ? new Date(from) : defaultFrom}
+            initialDateTo={to ? new Date(to) : defaultTo}
+            onUpdate={handleUpdate}
+            align="start"
+            showCompare={false}
+        />
     )
 }
